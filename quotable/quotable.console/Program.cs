92e9323be308453ea49d20db38d90bd19501938f
Breanna Garan <bgaran21@jcu.edu>
2019-09-24 18:05:00 -0400

    changes

--- quotable/quotable.console/Program.cs
+++ quotable/quotable.console/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using quotable.core;
+using System;
+using System.Collections.Generic;
 
 namespace quotable.console
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            SimpleRandomQuoteProvider provider=new SimpleRandomQuoteProvider();
+            IEnumerable<string> quotes;
+           // RandomQuoteProvider provider=new SimpleRandomQuoteProvider();
+			Console.Write("Enter the number of quotes: ");
+            var numQuotes = Console.ReadLine();
+            quotes= provider.getQuotes(long.Parse(numQuotes));
+            
+            foreach(var x in quotes)
+                {
+                Console.WriteLine(x);
+                }
+            
+           
+		}
+
+       
+			
     }
 }
---
+++ quotable/quotable.core/RandomQuoteProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace quotable.core
+{
+    public interface RandomQuoteProvider
+    {
+        IEnumerable<string> getQuotes(long numQuotes);
+    }
+}
