756e386f0a1d335e943c1f9ec7b2bfff31caa3bc
Breanna Garan <bgaran21@jcu.edu>
2019-09-24 21:43:44 -0400

    changes

--- quotable/quotable.core/DefaultRandomQuoteProvider.cs
+++ quotable/quotable.core/DefaultRandomQuoteProvider.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 
 namespace quotable.core
+
 {
     public class DefaultRandomQuoteProvider : RandomQuoteProvider
     {
-        IEnumerable<string> getQuotes(long numQuotes)
-        {
+        string[] quoteList = new string[] { "You’re off to great to places. Today is your day. Your mountain is waiting. So get on your way",
+            "Unless someone like you cares a whole awful lot. Nothing is going to get better. It’s not",
+            "It is better to know how to learn than to know" };
 
+        public IEnumerable<string> getQuotes(long numQuotes)
+        {
+            string[] returnList = new string[numQuotes];
+            for (int i = 0; i < numQuotes; i++)
+            {
+                Random random = new Random();
+                int randomQuote = random.Next(0, 3);
+                returnList[i] = quoteList[randomQuote];
+            }
+            IEnumerable<string> returnQuotes = returnList;
+            return returnQuotes;
         }
+
     }
 }
+
