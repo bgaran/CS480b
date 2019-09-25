2bcd85e0120de164ab2ac2a3fd3e0107ed69f76f
Breanna Garan <bgaran21@jcu.edu>
2019-09-24 21:42:09 -0400

    changes

---
+++ quotable/quotable.core/DefaultRandomQuoteProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace quotable.core
+{
+    public class DefaultRandomQuoteProvider : RandomQuoteProvider
+    {
+        IEnumerable<string> getQuotes(long numQuotes)
+        {
+
+        }
+    }
+}
