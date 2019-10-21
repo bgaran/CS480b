1861827fcbc128221f4995d8e5f8980e6765a193
Breanna Garan <bgaran21@jcu.edu>
2019-10-21 16:31:24 -0400

    changes

--- quotable/quotable.api/Startup.cs
+++ quotable/quotable.api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using quotable.core;
 
 namespace quotable
 {
@@ -24,6 +25,7 @@ namespace quotable
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RandomQuoteProvider, SimpleRandomQuoteProvider>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
